b305556b77cea23e2db779e1de3c9f7230affaae
alijoonn2443s-glitch <alijoonn2443s@gmail.com>
2026-09-15 14:08:47 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME='focusplan-v82';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html'])));self.skipWaiting()});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE_NAME).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match('./index.html')))));
