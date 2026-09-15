db1a164fe1e4a27e648b5fca4449780ef6ade4f8
alijoonn2443s-glitch <alijoonn2443s@gmail.com>
2026-09-15 23:54:51 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='focusplan-github-v101';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,x));return r}).catch(()=>caches.match('./index.html'))))});
